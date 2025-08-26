46a6f26d94c9aa36e1e9bbb2aa92112155c861a9
shihab-63 <thecodzhub@gmail.com>
2025-08-26 14:54:46 +0600

    toggling add future done

--- javaScript/home.js
+++ javaScript/home.js
@@ -7,35 +7,54 @@ function handleToggle (id) {
     document.getElementById(id).style.display = 'block';
 }
 
+// Reusable Color Toggling
+function colorToggling (id) {
+    const colorBtns = document.getElementsByClassName('color-btn');
+    for (const colorbtn of colorBtns) {
+        colorbtn.classList.remove('border-[#0874f2]', 'bg-[#0874f20d]');
+        colorbtn.classList.add('border-gray-300');
+    }
+    document.getElementById(id).classList.remove('border-gray-300');
+    document.getElementById(id).classList.add('border-[#0874f2]', 'bg-[#0874f20d]');
+}
+
+
+
 // Toggleing All Button
 // Add money btn
 document.getElementById('add-money-btn').addEventListener('click', function() {
     handleToggle('add-money-parent');
+    colorToggling('add-money-btn');
 });
 
 // cash-out-btn 
 document.getElementById('cash-out-btn').addEventListener('click', function () {
     handleToggle('cash-out-parent');
+    colorToggling('cash-out-btn');
 })
 
 // Transfer Money
 document.getElementById('transfer-money-btn').addEventListener('click', function () {
     handleToggle('transfer-money-parent');
+    colorToggling('transfer-money-btn')
 })
 
 // Get Bonus
 document.getElementById('get-bonus-btn').addEventListener('click', function () {
     handleToggle('get-bonus-parent');
+    colorToggling('get-bonus-btn')
 })
 
 // Pay Bill
 document.getElementById('pay-bill-btn').addEventListener('click', function () {
     handleToggle('pay-bill-parent');
+    colorToggling('pay-bill-btn')
 })
 
 // Transactions
 document.getElementById('transaction-btn').addEventListener('click', function () {
     handleToggle('transaction-history-parent');
+    colorToggling('transaction-btn')
 })
 
 
